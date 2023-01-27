226218ad94f3194fdcd6807cd61177684e3577d1
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-27 21:43:00 +0500

    create operation

--- script.ts
+++ script.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+	// const user = await prisma.user.create({
+	// 	data: {
+	// 		name: 'Khan',
+	// 		email: 'Khan@gmail.com',
+	// 	},
+	// });
+	// console.log(user);
+}
 
 main()
 	.catch((e) => {
