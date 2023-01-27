677b07d0d1604577ef73d01cdb2a656c711fb885
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-28 01:37:48 +0500

    basic

--- script.ts
+++ script.ts
@@ -2,7 +2,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-	// await prisma.user.deleteMany();
+	// const user = await prisma.user.create({
+	// 	data: {
+	// 		name: 'Junaid',
+	// 		email: 'junaidhkn@gmail.com',
+	// 		age: 22,
+	// 	},
+	// });
+	// console.log(user);
 }
 
 main()
